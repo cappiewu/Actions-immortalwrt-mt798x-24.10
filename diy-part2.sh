53639653bc56c4834cdb1935ff77719d71821df2
cappiewu <cappiewu@gmail.com>
2025-06-22 22:51:28 +0800

    Action环境初始化时安装zstd
    添加磊科N60 Pro灯的默认设置
    调整Ksmbd到NAS网络存储菜单
    临时调整netifd为最新版本
    更新config，去除打包imagebuilder和toolchain选项

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,6 +16,44 @@
 # 修改默认IP
 sed -i 's/192.168.6.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
+
+# 添加磊科N60 Pro灯的默认设置
+cat << 'EOF' > /tmp/insert_content
+while uci -q delete system.@led[0]; do :; done
+
+uci add system led
+uci set system.@led[-1].name='SysLoad'
+uci set system.@led[-1].sysfs='blue:power'
+uci set system.@led[-1].trigger='heartbeat'
+
+uci add system led
+uci set system.@led[-1].name='WAN'
+uci set system.@led[-1].sysfs='blue:wan'
+uci set system.@led[-1].trigger='netdev'
+uci set system.@led[-1].dev='wan'
+uci add_list system.@led[-1].mode='tx'
+uci add_list system.@led[-1].mode='rx'
+
+uci add system led
+uci set system.@led[-1].name='USB'
+uci set system.@led[-1].sysfs='blue:usb'
+uci set system.@led[-1].trigger='usbport'
+uci add_list system.@led[-1].port='usb1-port2'
+
+uci add system led
+uci set system.@led[-1].name='WLAN'
+uci set system.@led[-1].sysfs='blue:wlan'
+uci set system.@led[-1].trigger='netdev'
+uci set system.@led[-1].dev='rax0'
+uci add_list system.@led[-1].mode='tx'
+uci add_list system.@led[-1].mode='rx'
+
+uci commit
+
+EOF
+sed -i '2r /tmp/insert_content' package/emortal/default-settings/files/99-default-settings-chinese
+
+
 # 修改默认主机名
 #sed -i "s/hostname='.*'/hostname='N60Pro'/g" package/base-files/files/bin/config_generate
 
@@ -42,3 +80,12 @@ sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-passwall/luasrc/view/
 sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-passwall/luasrc/view/passwall/node_list/*.htm
 sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-passwall/luasrc/view/passwall/rule/*.htm
 sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-passwall/luasrc/view/passwall/server/*.htm
+
+# 调整Ksmbd到NAS网络存储菜单
+sed -i 's/services/nas/g' feeds/luci/applications/luci-app-ksmbd/root/usr/share/luci/menu.d/luci-app-ksmbd.json
+
+# 不知道什么原因，padavanonly在2025/6/20的提交27c0f700aedc8546f53b0d41eca67945766b70ca中，把netifd改为了旧版
+# 临时调整netifd为最新版本，等待padavanonly更新
+sed -i 's/2024-12-17/2025-05-23/g' package/network/config/netifd/Makefile
+sed -i 's/ea01ed41f3212ecbe000422f3c122a01b93fe874/7901e66c5f273bceee8981bc8a0c8b0e60945f60/g' package/network/config/netifd/Makefile
+sed -i 's/dbaad26c1f9b15d0caff6ccdf80d85b34d96bda72cbd2b1dc188a04136d96c28/8b85ec64e446ae065b1466c520b2d3aae329b6167221e425af903777278f557e/g' package/network/config/netifd/Makefile
